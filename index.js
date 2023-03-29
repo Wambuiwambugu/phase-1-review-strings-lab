921aadc4e890c50df0f5c833c68e7521ce329075
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-03-29 18:58:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Wambui Wambugu";
+const firstInitial = currentUser[0];;
+const restOfName = currentUser.slice(1);
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, " + firstInitial + "!";
